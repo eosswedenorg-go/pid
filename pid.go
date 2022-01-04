2eb56b7d748d9ad997f2a0765414b2073b5626e4
Henrik Hautakoski <henrik@eossweden.org>
2022-01-04 09:11:41 +0100

    Convert hard tabs to soft.

--- pid.go
+++ pid.go
@@ -2,26 +2,26 @@
 package pid
 
 import (
-	"os"
-	"strconv"
+    "os"
+    "strconv"
 )
 
 func Get() (int) {
-	return os.Getpid()
+    return os.Getpid()
 }
 
 func Save(path string) (error) {
 
-	fd, err := os.Create(path)
-	if err != nil {
-    	return err
-	}
+    fd, err := os.Create(path)
+    if err != nil {
+        return err
+    }
 
-	_, err = fd.WriteString(strconv.Itoa(Get()))
+    _, err = fd.WriteString(strconv.Itoa(Get()))
 
-	if err != nil {
-		return err
-	}
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
