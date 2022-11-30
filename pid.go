2991209a54977fcdb792166bf5f16021998fa03a
Henrik Hautakoski <henrik@eossweden.org>
2022-11-30 14:29:56 +0100

    Code format fixes.

--- pid.go
+++ pid.go
@@ -1,35 +1,32 @@
-
 package pid
 
 import (
-    "os"
-    "io/ioutil"
-    "strconv"
+	"io/ioutil"
+	"os"
+	"strconv"
 )
 
-func Get() (int) {
-    return os.Getpid()
+func Get() int {
+	return os.Getpid()
 }
 
-func Save(path string) (error) {
-
-    err := ioutil.WriteFile(path, []byte(strconv.Itoa(Get())), 0644)
-    if err != nil {
-        return err
-    }
-    return nil
+func Save(path string) error {
+	err := ioutil.WriteFile(path, []byte(strconv.Itoa(Get())), 0o644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func Read(path string) (int, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return -1, err
+	}
 
-    data, err := ioutil.ReadFile(path)
-    if err != nil {
-        return -1, err
-    }
-
-    pid, err := strconv.Atoi(string(data))
-    if err != nil {
-        return -1, err
-    }
-    return pid, nil
+	pid, err := strconv.Atoi(string(data))
+	if err != nil {
+		return -1, err
+	}
+	return pid, nil
 }
--- pid_test.go
+++ pid_test.go
@@ -1,91 +1,85 @@
-
 package pid
 
-import(
-    "os"
-    "io/ioutil"
-    "strconv"
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"testing"
 
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var osPid = os.Getpid()
 
 func TestGet(t *testing.T) {
-    pid := Get()
-    assert.Equal(t, pid, osPid)
+	pid := Get()
+	assert.Equal(t, pid, osPid)
 }
 
 func TestSave(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "go_test")
+	require.NoError(t, err)
 
-    tmpfile, err := ioutil.TempFile("", "go_test")
-    require.NoError(t, err)
+	defer os.Remove(tmpfile.Name())
 
-    defer os.Remove(tmpfile.Name())
+	err = Save(tmpfile.Name())
+	require.NoError(t, err)
 
-    err = Save(tmpfile.Name())
-    require.NoError(t, err)
+	data := make([]byte, 32)
+	l, err := tmpfile.Read(data)
+	require.NoError(t, err)
 
-    data := make([]byte, 32)
-    l, err := tmpfile.Read(data)
-    require.NoError(t, err)
+	pid, err := strconv.Atoi(string(data[:l]))
 
-    pid, err := strconv.Atoi(string(data[:l]))
-
-    require.NoError(t, err)
-    assert.Equal(t, pid, osPid)
+	require.NoError(t, err)
+	assert.Equal(t, pid, osPid)
 }
 
-
 func TestRead(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "go_test")
+	require.NoError(t, err)
 
-    tmpfile, err := ioutil.TempFile("", "go_test")
-    require.NoError(t, err)
+	defer os.Remove(tmpfile.Name())
 
-    defer os.Remove(tmpfile.Name())
+	_, err = tmpfile.WriteString("1234")
+	require.NoError(t, err)
 
-    _, err = tmpfile.WriteString("1234")
-    require.NoError(t, err)
+	pid, err := Read(tmpfile.Name())
 
-    pid, err := Read(tmpfile.Name())
-
-    require.NoError(t, err)
-    assert.Equal(t, pid, 1234)
+	require.NoError(t, err)
+	assert.Equal(t, pid, 1234)
 }
 
 func TestReadEmptyFile(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "go_test")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(tmpfile.Name())
 
-    tmpfile, err := ioutil.TempFile("", "go_test")
-    if err != nil {
-        t.Error(err)
-        return
-    }
-    defer os.Remove(tmpfile.Name())
+	pid, err := Read(tmpfile.Name())
 
-    pid, err := Read(tmpfile.Name())
-
-    require.Error(t, err)
-    assert.Equal(t, pid, -1)
+	require.Error(t, err)
+	assert.Equal(t, pid, -1)
 }
 
 func TestReadRandomContent(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "go_test")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(tmpfile.Name())
 
-    tmpfile, err := ioutil.TempFile("", "go_test")
-    if err != nil {
-        t.Error(err)
-        return
-    }
-    defer os.Remove(tmpfile.Name())
+	_, err = tmpfile.WriteString("some_random_data")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-    _, err = tmpfile.WriteString("some_random_data")
-    if err != nil {
-        t.Error(err)
-        return
-    }
-
-    pid, err := Read(tmpfile.Name())
-    require.Error(t, err)
-    assert.Equal(t, pid, -1)
+	pid, err := Read(tmpfile.Name())
+	require.Error(t, err)
+	assert.Equal(t, pid, -1)
 }
